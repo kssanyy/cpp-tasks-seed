80b1f6e1bd0ddb4f18329ae3cbf42625a714aa0c
kssanyy <odintpetrov@gmail.com>
2026-04-27 23:06:16 +0300

    BubbleSort and QuickSort uploaded

--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -10,12 +10,54 @@
 template <typename Iterator>
 void bubble_sort(Iterator begin, Iterator end)
 {
+    if (begin == end) {
+        return;
+    }
+
+    for (auto i = begin; i != end; ++i) {
+        for (auto j = begin; j != end - 1; ++j) {
+            if (*(j + 1) < *j) {
+                using std::swap;
+                swap(*j, *(j + 1));
+            }
+        }
+    }
 }
 
 // --------------------------- QuickSort (например) ---------------------------------------
 template <typename Iterator>
+Iterator separation(Iterator left, Iterator right) {
+    auto opr = *(right - 1);
+    auto i = left;
+
+    for (auto j = left; j != right - 1; ++j) {
+        if (!(opr < *j)) {
+            using std::swap;
+            swap(*i, *j);
+            ++i;
+        }
+    }
+
+    using std::swap;
+    swap(*i, *(right - 1));
+    return i;
+}
+
+template <typename Iterator>
+void quick_sort_impl(Iterator left, Iterator right) {
+    if (left < right - 1) {
+        auto fence = separation(left, right);
+        quick_sort_impl(left, fence);
+        quick_sort_impl(fence + 1, right);
+    }
+}
+
+template <typename Iterator>
 void quick_sort(Iterator begin, Iterator end)
 {
+    if (begin < end) {
+        quick_sort_impl(begin, end);
+    }
 }
 
 #endif // SORTING_H
